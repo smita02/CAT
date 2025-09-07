95c66eef14e852c2fcc98e23d335ee46a0674d8b
smitavqa <smitav.qa@gmail.com>
2025-09-07 16:09:03 +0530

    C java file commited

---
+++ src/main/java/Pages/C.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("I love GitBash and github");
+	}
+
+}

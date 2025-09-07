7306dbb239cfd93f2619316b3645d52a8d5bede7
smitavqa <smitav.qa@gmail.com>
2025-09-07 16:34:31 +0530

    -update C file

--- src/main/java/Pages/C.java
+++ src/main/java/Pages/C.java
@@ -5,6 +5,7 @@ public class C {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("I love GitBash and github");
+     System.out.println("this is master branch");
 	}
 
 }

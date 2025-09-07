87e7c9903be5bfab03b16858d8416ab37f7c31f7
smitavqa <smitav.qa@gmail.com>
2025-09-07 17:04:25 +0530

    A class changes from master

--- src/main/java/Pages/A.java
+++ src/main/java/Pages/A.java
@@ -3,7 +3,8 @@ package Pages;
 public class A {
 
 	public static void main(String[] args) {
-	System.out.println(" Lucky me ....Smita");
+    System.out.println("Master Brnach");	
+	System.out.println(" Lucky me ....Smita...");
 	}
 
 }

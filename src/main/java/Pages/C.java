22844dab248ca465796694b703e56d5b8bb58c4b
smitavqa <smitav.qa@gmail.com>
2025-09-07 16:38:01 +0530

    updateing c file in feature branch

--- src/main/java/Pages/C.java
+++ src/main/java/Pages/C.java
@@ -5,7 +5,8 @@ public class C {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("I love GitBash and github");
-     System.out.println("this is master branch");
+     System.out.println("this is feature branch");
+  
 	}
 
 }

14dd47133806bf49dd8a45f3b14def57bdfebe0f
smitavqa <smitav.qa@gmail.com>
2025-09-07 23:08:02 +0530

    A1

--- src/main/java/Pages/A.java
+++ src/main/java/Pages/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
     System.out.println("Master Brnach");	
 	System.out.println(" Lucky me ....Smita...");
+	System.out.println(" Success is on the way");
 	}
 
 }

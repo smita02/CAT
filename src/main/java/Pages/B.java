36f00657bcab1788e936bd69305c93bfca8d3fa3
prafullb007 <prafullb007@gmail.com>
2025-09-07 19:34:28 +0530

    fd1

--- src/main/java/Pages/B.java
+++ src/main/java/Pages/B.java
@@ -4,7 +4,7 @@ public class B {
 
 	public static void main(String[] args) {
    System.out.println("Master Excellet performance");
-   System.out.println("Feature Excellet performance Prafull");
+   System.out.println("Feature Excellet performance Prafull Smita");
 	}
 
 }
